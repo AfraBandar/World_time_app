38fb8bdd9d644b1ccf2af39d216a62be39ee7d26
ash <fitoash666@gmail.com>
2021-12-14 13:02:45 +0300

    created world_time.dart

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:world_time_app/services/world_time.dart';
 
 
 class Loading extends StatefulWidget {
@@ -10,35 +11,33 @@ class Loading extends StatefulWidget {
 }
 
 class _State extends State<Loading> {
-  void getTime() async {
-    //get response
-   Response response = await get(Uri.parse('http://worldtimeapi.org/api/timezone/Europe/London'));
-   Map data =  jsonDecode(response.body);
 
-   //get proprties from data
-    String datetime = data['utc_datetime'];
-    String offset = data['utc_offset'].substring(1,3);//create a sub string from the offset string to only have the number and not the + sign
-    //print(offset);
-    //print(datetime);
+  String time1 = 'loading';//initial value until the getTime data is complete
 
-    //create a datetime object
-    DateTime now = DateTime.parse(datetime);//converts the time to a date time object
-    now = now.add(Duration(hours:int.parse(offset)));//adds the offset that has been cut short to only the number
-    //and then converted to an int
-    print(now);
-
-  }
+  void setupWorldTime() async {
+    WorldTime instance = WorldTime ('London','london.png','Europe/London');
+    await instance.getTime();
+    print(instance.time);
+    
+    setState(() {
+      time1 = instance.time;
+    });// to refresh the time variable after getting the data
+    
+  }//create an object instance to connect the getTime function to this page
 
   @override
   void initState() {
     super.initState();//super runs the inherted function that is being override then run the extra code
     //written inside the initstate
-    getTime();
+    setupWorldTime();
   }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text('LOADING SCREEN'),
+      body: Padding(
+        padding: EdgeInsets.all(50.0),
+        child:Text(time1),
+      ),
     );
   }
 }
--- lib/services/world_time.dart
+++ lib/services/world_time.dart
@@ -2,11 +2,44 @@ import 'package:http/http.dart' ;
 import 'dart:convert';
 
 class WorldTime{
-  String location; //location name for th ui
-  String time; //the time in the location
-  String flag; // a url to an asset flag icon
-  String url; //location url for api endpoint
+  late String location; //location name for the ui
+  late String time; //the time in the location
+  late String flag; // a url to an asset flag icon
+  late String url; //location url for api endpoint exp ( /Europe/london)
 
+  WorldTime (String location, String flag, String url) {
+    this.location = location;
+    this.flag = flag;
+    this.url=url;
+  }//constructer
 
+  Future <void> getTime() async {
+
+    try {
+      //get response
+      Response response = await get(Uri.parse('http://worldtimeapi.org/api/timezone/$url')); //getting the data from the json link then storing the response in a response variable
+      Map data =  jsonDecode(response.body); //pass the data from the response and then from the body of the response wich is where the actual data is stored
+
+      //get proprties from data
+      String datetime = data['utc_datetime'];
+      String offset = data['utc_offset'].substring(1,3);//create a sub string from the offset string to only have the number and not the + sign
+      //print(offset);
+      //print(datetime);
+
+      //create a datetime object
+      DateTime now = DateTime.parse(datetime);//converts the time to a date time object
+      now = now.add(Duration(hours:int.parse(offset)));//adds the offset that has been cut short to only the number
+      //and then converted to an int
+
+      //set time property
+      time = now.toString(); //convert the datetime object into a string then store it in the time variable
+
+    }
+    catch (e) {
+      print ('caught error: $e');
+      time = 'could not get time data';
+    }
+
+  }
 
 }
